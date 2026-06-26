703fad240dbca7bea77f4b02f658b54bf8166f60
Deepika B <deepikab.23it@kongu.edu>
2026-06-26 14:08:05 +0530

    Time: 2 ms (99.9%), Space: 52.5 MB (21.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -24,7 +24,3 @@ class Solution {
         return cnt;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
