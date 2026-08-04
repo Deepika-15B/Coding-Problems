1c3374a0124baadea68a0d1a5d2efc2e50eb402d
Deepika B <deepikab.23it@kongu.edu>
2026-08-04 20:31:23 +0530

    Time: 20 ms (82.99%), Space: 56.4 MB (77.39%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l = 0;
+        int r = people.length - 1;
+        int b = 0;
+        
+        while(l<=r)
+        {
+            if(people[l] + people[r]<=limit)
+            {
+                l++;
+                r--;
+                b++;
+            }
+            else
+            {
+                r--;
+                b++;
+            }
+        }
+        return b;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
