cde8f3b7fdf8e7efc8962b5ef4e1290b97257d90
Deepika B <deepikab.23it@kongu.edu>
2026-08-13 09:31:41 +0530

    Time: 0 ms (100%), Space: 13.2 MB (53.02%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range (len(nums)):
+            current = nums[i]
+            needed = target - current
+
+            if needed in seen:
+                return (seen[needed],i)
+
+            seen[current] = i        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
