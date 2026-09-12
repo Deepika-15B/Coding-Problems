71a918138c2ac3120c1bab81e0029a5c411279fc
Deepika B <deepikab.23it@kongu.edu>
2026-09-12 10:06:38 +0530

    Time: 466 ms (48.7%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+Select Distinct author_id as id 
+From Views
+Where author_id=viewer_id
+Order by id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
