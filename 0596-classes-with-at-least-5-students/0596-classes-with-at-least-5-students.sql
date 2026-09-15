a56a1be8dee70438bd841e49099382d58d2db8bc
Deepika B <deepikab.23it@kongu.edu>
2026-09-15 12:24:30 +0530

    Time: 381 ms (35.81%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT class from Courses GROUP BY class having COUNT(student) >=5;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
